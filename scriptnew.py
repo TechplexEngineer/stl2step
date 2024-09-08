220309ad821e8b4740d31b7e9327c878dd133c38
Blake Bourque <techplex.engineer@gmail.com>
2024-09-08 09:59:33 -0400

    Removed unneeded names

--- scriptnew.py
+++ scriptnew.py
@@ -18,36 +18,28 @@ print("Begin command Part_ShapeFromMesh")
 partShape = Part.Shape()
 partShape.makeShapeFromMesh(meshObj.Mesh.Topology, 0.001, True)
 
-partObj = doc.addObject('Part::Feature', 'Half_Inch_Hex_Bridgeport_Adapter001')
+partObj = doc.addObject('Part::Feature')
 partObj.Shape = partShape
 partObj.purgeTouched()
-# del partShape
 print("End command Part_ShapeFromMesh")
 print()
 
+
 print("Begin command Part_RefineShape")
-refinedObj = doc.addObject('Part::Refine','Half_Inch_Hex_Bridgeport_Adapter_refined')
+refinedObj = doc.addObject('Part::Refine')
 refinedObj.Source = partObj
-# refinedObj.Label = partObj.Label
 doc.recompute()
 print("End command Part_RefineShape")
 print()
 
 
-
-
 print("Begin command Part_MakeSolid")
-__s__=refinedObj.Shape
-__s__=Part.Solid(__s__)
-__o__=FreeCAD.ActiveDocument.addObject("Part::Feature","Half_Inch_Hex_Bridgeport_Adapter002_solid")
-# __o__.Label="Half_Inch_Hex_Bridgeport_Adapter002 (Solid)"
-__o__.Shape=__s__
-# del __s__, __o__
+solidObj= FreeCAD.ActiveDocument.addObject("Part::Feature")
+solidObj.Shape = Part.Solid(refinedObj.Shape)
 print("End command Part_MakeSolid")
 print()
 
 
-
 print("Begin command Std_Export")
-Part.export([doc.getObject("Half_Inch_Hex_Bridgeport_Adapter002_solid")], step_file)
+Part.export([solidObj], step_file)
 print("Done!")
